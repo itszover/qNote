fdcfd9f9e296ea055260e006ed3bc33cb080f1e6
itszover <jose.martinho@outlook.com.br>
2025-09-30 16:45:07 -0300

    refactor readline function and update file handling in main

--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 #include <stdint.h>
 #include <string.h>
 
-char *readline(void) {
+char *readline(void)
+{
   size_t cap = 16;
   size_t length = 0;
   char *buffer = malloc(cap);
   int c;
 
-  while ((c = getchar()) != EOF && c != '\n') {
-    if (length + 1 >= cap) {
-      if (cap > SIZE_MAX / 2) {
+  while ((c = getchar()) != EOF && c != '\n')
+  {
+    if (length + 1 >= cap)
+    {
+      if (cap > SIZE_MAX / 2)
+      {
         free(buffer);
         return NULL;
       }
@@ -21,7 +25,8 @@ char *readline(void) {
     buffer[length++] = (char)c;
   }
 
-  if (c == EOF && length == 0) {
+  if (c == EOF && length == 0)
+  {
     free(buffer);
     return NULL;
   }
@@ -30,13 +35,41 @@ char *readline(void) {
   return buffer;
 }
 
-int main(int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--read") == 0) {
-      FILE *f = fopen("output.txt", "r");
+int main(int argc, char **argv)
+{
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--read") == 0)
+    {
+      FILE *f = fopen("p0.txt", "r");
       int c;
-      while ((c = fgetc(f)) != EOF) {
+      while ((c = fgetc(f)) != EOF)
+      {
         putchar(c);
+      }
+      exit(EXIT_SUCCESS);
+    }
+
+    if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--new") == 0)
+    {
+      char filename[7];
+
+      for (int i = 0; i < 1000; i++)
+      {
+        sprintf(filename, "p%d.txt", i);
+        FILE *f = fopen(filename, "r");
+        if (f == NULL)
+        {
+          printf("Creating new file: %s\ni=%d\n", filename, i);
+          sprintf(filename, "p%d.txt", i);
+          FILE *s = fopen(filename, "w");
+          printf(">>>\n");
+          char *line = readline();
+          fprintf(s, "%s\n", line);
+          fclose(f);
+          exit(EXIT_SUCCESS);
+        }
+        fclose(f);
       }
       exit(EXIT_SUCCESS);
     }
